802be08a4218fd083249b616b8e7d086d2d4cc9b
selfty <vojta.mach@gmail.com>
2020-11-30 18:45:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/gameapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.gameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
